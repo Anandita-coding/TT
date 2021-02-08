fd1b8c08419f5e70934247c337f7f7e22b7dd8ea
Anandita-coding <71819248+Anandita-coding@users.noreply.github.com>
2021-02-08 15:11:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var hr,mn,sc;
+
+
+function setup() {
+  createCanvas(600,600);
+  
+  hr = hour();
+  mn = minute();
+  sc = second();
+
+  angleMode(DEGREES);
+
+  
+}
+
+function draw() {
+  background("pink");  
+  
+  scAngle = map(sc,0,60,0,360)
+  mnAngle = map(mn,0,60,0,360)
+  hrAngle = map(hr,0,12,0,360)
+
+  push();
+  rotate(scAngle)
+  stroke(0,0,255)
+  strokeWeight(7)
+  translate(600,300)
+  line(10,400,100,100)
+  pop();
+
+  push();
+  rotate(mnAngle)
+  stroke(0,0,255)
+  strokeWeight(7)
+  line(300,0,100,0)
+  pop();
+
+  push();
+  rotate(hrAngle)
+  stroke(0,0,255)
+  strokeWeight(7)
+  line(300,0,100,0)
+  pop();
+
+
+  drawSprites();
+}
