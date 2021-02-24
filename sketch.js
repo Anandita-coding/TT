5bc9cd0949dc54f24c6cf584df439f8081ec6d0a
Anandita-coding <71819248+Anandita-coding@users.noreply.github.com>
2021-02-24 14:39:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,13 +18,14 @@ function draw() {
   
   scAngle = map(sc,0,60,0,360)
   mnAngle = map(mn,0,60,0,360)
-  hrAngle = map(hr,0,12,0,360)
+  map(hr%12, 0,12, 0, 360)
+  
 
   push();
   rotate(scAngle)
   stroke(0,0,255)
   strokeWeight(7)
-  translate(600,300)
+  translate(300,300)
   line(10,400,100,100)
   pop();
 
@@ -32,13 +33,15 @@ function draw() {
   rotate(mnAngle)
   stroke(0,0,255)
   strokeWeight(7)
-  line(300,0,100,0)
+  translate(300,300)
+  line(10,0,100,0)
   pop();
 
   push();
   rotate(hrAngle)
   stroke(0,0,255)
   strokeWeight(7)
+  translate(300,300)
   line(300,0,100,0)
   pop();
 
